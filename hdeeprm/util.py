56389e73a081d470914f629207d11650ac7584b5
Esteban Stafford <esteban.stafford@unican.es>
2021-03-18 18:26:50 +0100

    Removed XML generation

--- hdeeprm/util.py
+++ hdeeprm/util.py
@@ -11,7 +11,6 @@ import defusedxml.minidom as mxml
 import numpy
 import numpy.random as nprnd
 import resource as res
-from __xml__ import exml, XMLElement
 import logging
 
 min_speed = 0.005
@@ -59,11 +58,7 @@ Args:
     return job_limits, workload
 
 def generate_platform(platform_name: str,platform_file_path: str, platform_library_path: str) -> (dict, list):
-    """HDeepRM JSON Platform -> Batsim-ready XML format + Resource Hierarchy.
-
-Parses a HDeepRM JSON formatted platform definition and outputs both a Batsim-ready XML file
-and the Resource Hierarchy pickled for the Decision System to understand relations between Cores,
-Processors and Nodes.
+    """ Construct platform definition from platform name, file and library.
 
 Args:
     platform_file_path (str):
@@ -77,12 +72,10 @@ Args:
     shared_state = {
         # Type of resources in the Platform
         'types': None,
-        'gen_platform_xml': False,
         'gen_res_hierarchy': True,
         'counters': {'cluster': 0, 'node': 0, 'processor': 0, 'core': 0},
         # Core pool for filtering and selecting Cores is initially empty
         'core_pool': [],
-        'cluster_xml': None,
         # Need to temporarly store information about up / down routes, since the XML DTD spec
         # imposes setting them at the end of generating all nodes. Initially, these are empty
         'udlink_routes': []
@@ -91,15 +84,9 @@ Args:
     root_desc = shared_state['types']['platform'][platform_name]
     print(f'Using platform {platform_name}')
     root_el = None
-    main_zone_xml = None
-    if shared_state['gen_platform_xml']:
-        main_zone_xml = _root_xml()
     if shared_state['gen_res_hierarchy']:
         root_el = _root_el()
-    _generate_clusters(shared_state, root_desc, root_el, main_zone_xml)
-    if shared_state['gen_platform_xml']:
-        _global_links(shared_state, root_desc, main_zone_xml)
-        _zone_routes(root_desc, main_zone_xml)
+    _generate_clusters(shared_state, root_desc, root_el)
 
     return root_el, shared_state
 
@@ -121,20 +108,6 @@ def _load_data(platform_file_path: str, platform_library_path: str) -> dict:
               types[group].update(types_from_file[group]);
     return types
 
-def _root_xml() -> dict:
-    # Generates the 'platform' and 'main zone' XML elements. These contain the 'master zone' and all
-    # the clusters.
-    root_xml = exml.Element('platform', attrib={'version': '4.1'})
-    main_zone_xml = exml.SubElement(root_xml, 'zone', attrib={'id': 'main', 'routing': 'Full'})
-    # Master zone and master host. Master host is not taken into account in energy measures
-    exml.SubElement(
-        exml.SubElement(
-            exml.SubElement(main_zone_xml,
-                            'zone', attrib={'id': 'master', 'routing': 'None'}),
-            'host', attrib={'id': 'master_host', 'speed': '1Gf'}),
-        'prop', attrib={'id': 'watt_per_state', 'value': '0.0:0.0'})
-    return main_zone_xml
-
 def _root_el() -> dict:
     # Resource hierarchy starts on the Platform element
     return {
@@ -144,28 +117,13 @@ def _root_el() -> dict:
         'clusters': []
     }
 
-def _generate_clusters(shared_state: dict, root_desc: dict, root_el: dict,
-                       main_zone_xml: XMLElement) -> None:
+def _generate_clusters(shared_state: dict, root_desc: dict, root_el: dict) -> None:
     for cluster_desc in root_desc['clusters']:
         cluster_el = None
-        if shared_state['gen_platform_xml']:
-            shared_state['cluster_xml'] = _cluster_xml(shared_state, main_zone_xml)
         if shared_state['gen_res_hierarchy']:
             cluster_el = _cluster_el(root_el)
         _generate_nodes(shared_state, cluster_desc, cluster_el)
-        if shared_state['gen_platform_xml']:
-            # All hosts have been generated, now create the up / down routes
-            _udlink_routes(shared_state)
         shared_state['counters']['cluster'] += 1
-
-def _cluster_xml(shared_state: dict, main_zone_xml: XMLElement) -> XMLElement:
-    cluster_xml = exml.SubElement(
-        main_zone_xml, 'zone',
-        attrib={'id': f'clu_{shared_state["counters"]["cluster"]}', 'routing': 'Full'})
-    # Each cluster has a router to communicate to other clusters and the master zone
-    exml.SubElement(cluster_xml, 'router',
-                    attrib={'id': f'rou_{shared_state["counters"]["cluster"]}'})
-    return cluster_xml
 
 def _cluster_el(root_el: dict) -> dict:
     cluster_el = {
@@ -179,23 +137,10 @@ def _generate_nodes(shared_state: dict, cluster_desc: dict, cluster_el: dict) ->
     for node_desc in cluster_desc['nodes']:
         for _ in range(node_desc['number']):
             node_el = None
-            if shared_state['gen_platform_xml']:
-                _node_xml(shared_state, cluster_desc)
             if shared_state['gen_res_hierarchy']:
                 node_el = _node_el(shared_state, node_desc, cluster_el)
             _generate_processors(shared_state, node_desc, node_el)
             shared_state['counters']['node'] += 1
-
-def _node_xml(shared_state: dict, cluster_desc: dict) -> None:
-    # Create the node UP/DOWN link. SPLITDUPLEX model is utilized for simulating TCP connections
-    # characteristics
-    udlink_attrs = {'id': f'udl_{shared_state["counters"]["node"]}',
-                    'sharing_policy': 'SPLITDUPLEX'}
-    # Bandwidth
-    udlink_attrs.update(shared_state['types']['network'][cluster_desc['local_links']['type']])
-    # Latency
-    udlink_attrs.update({'latency': cluster_desc['local_links']['latency']})
-    exml.SubElement(shared_state['cluster_xml'], 'link', attrib=udlink_attrs)
 
 def _node_el(shared_state: dict, node_desc: dict, cluster_el: dict) -> dict:
     # Transform memory from GB to MB
@@ -281,21 +226,9 @@ def _proc_el(shared_state: dict, proc_desc: dict, node_el: dict, gflops_per_core
 def _generate_cores(shared_state: dict, gflops_per_core_xml: dict, power_per_core_xml: str,
                     proc_desc: dict, proc_el: dict, p_state_with_speed:list) -> None:
     for _ in range(shared_state['types']['processor'][proc_desc['type']]['cores']):
-        if shared_state['gen_platform_xml']:
-            _core_xml(shared_state, gflops_per_core_xml, power_per_core_xml)
         if shared_state['gen_res_hierarchy']:
             _core_el(shared_state, proc_el, proc_desc, p_state_with_speed)
         shared_state['counters']['core'] += 1
-
-def _core_xml(shared_state: dict, gflops_per_core_xml: dict, power_per_core_xml: str) -> None:
-    # Create the Core XML element and associate power properties
-    core_attrs = {'id': f'cor_{shared_state["counters"]["core"]}', 'pstate': f'{res.number_p_states+1}'}
-    core_attrs.update(gflops_per_core_xml)
-    exml.SubElement(exml.SubElement(shared_state['cluster_xml'], 'host', attrib=core_attrs),
-                    'prop', attrib={'id': 'watt_per_state', 'value': power_per_core_xml})
-    # Append the up / down route parameters for generating after all the Cores
-    shared_state['udlink_routes'].append((f'cor_{shared_state["counters"]["core"]}',
-                                          f'udl_{shared_state["counters"]["node"]}'))
 
 def _core_el(shared_state: dict, proc_el: dict, proc_desc: dict, p_state_with_speed:list) -> None:
     proccesor = shared_state['types']['processor'][proc_desc['type']]
@@ -309,50 +242,3 @@ def _core_el(shared_state: dict, proc_el: dict, proc_desc: dict, p_state_with_sp
     proc_el['node']['cluster']['platform']['total_cores'] += 1
     proc_el['local_cores'].append(core_el)
     shared_state['core_pool'].append(core_el)
-
-def _udlink_routes(shared_state: dict) -> None:
-    for udlink_route in shared_state['udlink_routes']:
-        core_id, udlink_id = udlink_route
-        udlink_route_down_xml = exml.SubElement(
-            shared_state['cluster_xml'], 'route',
-            attrib={'src': f'rou_{shared_state["counters"]["cluster"]}', 'dst': core_id,
-                    'symmetrical': 'NO'})
-        exml.SubElement(
-            udlink_route_down_xml, 'link_ctn', attrib={'id': udlink_id, 'direction': 'DOWN'})
-        udlink_route_up_xml = exml.SubElement(
-            shared_state['cluster_xml'], 'route',
-            attrib={'src': core_id, 'dst': f'rou_{shared_state["counters"]["cluster"]}',
-                    'symmetrical': 'NO'})
-        exml.SubElement(
-            udlink_route_up_xml, 'link_ctn', attrib={'id': udlink_id, 'direction': 'UP'})
-    # Clean the routes for next cluster
-    shared_state['udlink_routes'] = []
-
-def _global_links(shared_state: dict, root_desc: dict, main_zone_xml: XMLElement) -> None:
-    # Create a global link for each cluster for connecting to the master host
-    for cluster_n in range(len(root_desc['clusters'])):
-        global_link_attrs = {'id': f'glob_lnk_{cluster_n}', 'sharing_policy': 'SPLITDUPLEX'}
-        # Bandwidth
-        global_link_attrs.update(
-            shared_state['types']['network'][root_desc['global_links']['type']])
-        # Latency
-        global_link_attrs.update({'latency': root_desc['global_links']['latency']})
-        exml.SubElement(main_zone_xml, 'link', attrib=global_link_attrs)
-
-def _zone_routes(root_desc: dict, main_zone_xml: XMLElement) -> None:
-    # Create the zone routes over the global links
-    for cluster_n in range(len(root_desc['clusters'])):
-        zone_route_down_xml = exml.SubElement(
-            main_zone_xml, 'zoneRoute',
-            attrib={'src': 'master', 'dst': f'clu_{cluster_n}', 'gw_src': 'master_host',
-                    'gw_dst': f'rou_{cluster_n}', 'symmetrical': 'NO'})
-        exml.SubElement(
-            zone_route_down_xml, 'link_ctn',
-            attrib={'id': f'glob_lnk_{cluster_n}', 'direction': 'DOWN'})
-        zone_route_up_xml = exml.SubElement(
-            main_zone_xml, 'zoneRoute',
-            attrib={'src': f'clu_{cluster_n}', 'dst': 'master', 'gw_src': f'rou_{cluster_n}',
-                    'gw_dst': 'master_host', 'symmetrical': 'NO'})
-        exml.SubElement(
-            zone_route_up_xml, 'link_ctn',
-            attrib={'id': f'glob_lnk_{cluster_n}', 'direction': 'UP'})
