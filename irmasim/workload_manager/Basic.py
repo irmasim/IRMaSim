1fc2821d50bb5da90f964a3c35fe706378d860be
Esteban Stafford <esteban.stafford@unican.es>
2022-10-31 19:29:11 +0100

    Use resources in ascending order

--- irmasim/workload_manager/Basic.py
+++ irmasim/workload_manager/Basic.py
@@ -1,4 +1,5 @@
 from irmasim.Job import Job
+from irmasim.Task import Task
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from irmasim.Simulator import Simulator
@@ -8,8 +9,8 @@ class Basic:
 
     def __init__(self, simulator: 'Simulator'):
         self.simulator = simulator
-        self.idle_resources = self.simulator.get_resources_ids()
-        self.busy_resources = []
+        self.resources = [ [ resource_id, 1 ] for resource_id in self.simulator.get_resources_ids() ]
+        self.idle_resources = len(self.resources)
         self.pending_jobs = []
         self.running_jobs = []
 
@@ -21,19 +22,16 @@ class Basic:
     def on_job_completion(self, jobs: list):
         for job in jobs:
             for task in job.tasks:
-                self.busy_resources.remove(task.resource)
-                # TODO mantener orden recursos
-                self.idle_resources.insert(0, task.resource)
+                self.deallocate(task)
             self.running_jobs.remove(job)
         while self.schedule_next_job():
             pass
 
     def schedule_next_job(self):
-        if self.pending_jobs != [] and len(self.idle_resources) >= len(self.pending_jobs[0].tasks):
+        if self.pending_jobs != [] and self.idle_resources >= len(self.pending_jobs[0].tasks):
             next_job = self.pending_jobs.pop(0)
             for task in next_job.tasks:
-                task.allocate(self.idle_resources.pop(0))
-                self.busy_resources.append(task.resource)
+                self.allocate(task)
             self.simulator.schedule(next_job.tasks)
             self.running_jobs.append(next_job)
             return True
@@ -45,3 +43,19 @@ class Basic:
 
     def on_end_simulation(self):
         pass
+
+    def allocate(self, task: Task):
+        resource = 0
+        while self.resources[resource][1] == 0:
+            resource += 1
+        self.resources[resource][1] = 0
+        self.idle_resources -= 1
+        task.allocate(self.resources[resource][0])
+
+    def deallocate(self, task: Task):
+        for resource in range(len(self.resources)):
+           if self.resources[resource][1] == 0 and self.resources[resource][0] == task.resource:
+               self.resources[resource][1] = 1
+               self.idle_resources += 1
+               break
+
