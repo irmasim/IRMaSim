cc9ce5975571dc516e9dfe117bfa6d0ab3b5c576
Esteban Stafford <esteban.stafford@unican.es>
2022-11-02 14:14:26 +0100

    Rudimentary v2 node performance model
    Only applies CPU equation. Does not consider memory contention.

--- irmasim/platform/TaskRunner.py
+++ irmasim/platform/TaskRunner.py
@@ -2,15 +2,17 @@ from irmasim.platform.Resource import Resource
 from irmasim.platform.EnergyConsumer import EnergyConsumer
 from irmasim.Task import Task
 
-
 class TaskRunner(Resource, EnergyConsumer):
 
     def __init__(self, id: str, config: dict):
         super(TaskRunner, self).__init__(id=id, config=config)
 
     def schedule(self, task: Task, resource_id: list):
-        child = self.find_child(resource_id.pop(0))
-        child.schedule(task, resource_id)
+        try:
+            child = self.find_child(resource_id.pop(0))
+            child.schedule(task, resource_id)
+        except IndexError:
+            raise Exception('Cannot schedule a task to an empty id')
 
     def get_next_step(self):
         return min([child.get_next_step() for child in self.children])
@@ -20,8 +22,11 @@ class TaskRunner(Resource, EnergyConsumer):
             child.advance(delta_time)
 
     def reap(self, task: Task, resource_id: list):
-        child = self.find_child(resource_id.pop(0))
-        child.reap(task, resource_id)
+        try:
+            child = self.find_child(resource_id.pop(0))
+            child.reap(task, resource_id)
+        except IndexError:
+            raise Exception('Cannot reap a task from an empty id')
 
     def get_joules(self, delta_time: float):
         return sum([child.get_joules(delta_time) for child in self.children])
--- irmasim/platform/models/modelV2/Node.py
+++ irmasim/platform/models/modelV2/Node.py
@@ -1,7 +1,42 @@
 from irmasim.platform.BasicNode import BasicNode
+from irmasim.Task import Task
+import math
 
+
+class Process:
+    def __init__(self, node: 'Node', task: Task):
+        self.node = node
+        self.task = task
+        self.speedup = 1
+
+    def get_next_step(self):
+            return self.task.ops / (self.node.gops * 1e9 * self.speedup)
+
+    def advance(self, delta_time: float):
+        self.task.ops -= math.ceil(self.node.gops * 1e9 * self.speedup * delta_time)
 
 class Node (BasicNode):
 
     def __init__(self, id: list, config: dict):
         super(Node, self).__init__(id=id, config=config)
+        self.processes = []
+        self.gops = config['clock_rate']
+
+    def schedule(self, task: Task, resource_id: list):
+        self.processes.append(Process(self,task))
+
+    def get_next_step(self):
+        if self.processes == []:
+            return math.inf
+        else:
+            return min([process.get_next_step() for process in self.processes])
+
+    def advance(self, delta_time: float):
+        for process in self.processes:
+            process.advance(delta_time)
+
+    def reap(self, task: Task, resource_id: list):
+        for process in self.processes:
+            if process.task == task:
+                self.processes.remove(process)
+                break
