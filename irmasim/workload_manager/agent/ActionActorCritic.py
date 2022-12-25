2d0745e1c12aa5fd92a97fbf7d9df7322edabe31
jFompe <jaime.fompe@gmail.com>
2022-12-25 23:38:25 +0100

    Add obs and act to PPOBuffer

--- irmasim/workload_manager/agent/ActionActorCritic.py
+++ irmasim/workload_manager/agent/ActionActorCritic.py
@@ -138,8 +138,9 @@ class ActionCritic(nn.Module):
 
 class PPOBuffer:
 
-    def __init__(self, size, gamma=0.99, lam=0.97):
-        size = size * 100  # assume the traj can be really long
+    def __init__(self, obs_dim, act_dim, size, gamma=0.99, lam=0.97):
+        self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
         self.adv_buf = np.zeros(size, dtype=np.float32)
         self.rew_buf = np.zeros(size, dtype=np.float32)
         self.ret_buf = np.zeros(size, dtype=np.float32)
@@ -147,12 +148,12 @@ class PPOBuffer:
         self.logp_buf = np.zeros(size, dtype=np.float32)
         self.gamma, self.lam = gamma, lam
         self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+        self.rewards = []
 
-    def store(self, rew, val, logp) -> None:
-        """
-        Append one timestep of agent-environment interaction to the buffer.
-        """
+    def store(self, obs, act, rew, val, logp) -> None:
         assert self.ptr < self.max_size  # buffer has to have room so you can store
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
         self.rew_buf[self.ptr] = rew
         self.val_buf[self.ptr] = val
         self.logp_buf[self.ptr] = logp
@@ -162,23 +163,10 @@ class PPOBuffer:
         self.ptr -= 1
 
     def finish_path(self, last_val) -> None:
-        """
-        Call this at the end of a trajectory, or when one gets cut off
-        by an epoch ending. This looks back in the buffer to where the
-        trajectory started, and uses rewards and value estimates from
-        the whole trajectory to compute advantage estimates with GAE-Lambda,
-        as well as compute the rewards-to-go for each state, to use as
-        the targets for the value function.
-        The "last_val" argument should be 0 if the trajectory ended
-        because the agent reached a terminal state (died), and otherwise
-        should be V(s_T), the value function estimated for the last state.
-        This allows us to bootstrap the reward-to-go calculation to account
-        for timesteps beyond the arbitrary episode horizon (or epoch cutoff).
-        """
-
         path_slice = slice(self.path_start_idx, self.ptr)
         rews = np.append(self.rew_buf[path_slice], last_val)
         vals = np.append(self.val_buf[path_slice], last_val)
+        self.rewards = rews
 
         # GAE-Lambda advantage calculation
         deltas = rews[:-1] + self.gamma * vals[1:] - vals[:-1]
@@ -189,15 +177,17 @@ class PPOBuffer:
 
         self.path_start_idx = self.ptr
 
-    def get(self) -> tuple:
-        """
-        Call this at the end of an epoch to get all the data from
-        the buffer, with advantages appropriately normalized (shifted to have
-        mean zero and std one). Also, resets some pointers in the buffer.
-        """
-        assert self.ptr < self.max_size
-        actual_size = self.ptr
+    def get(self) -> dict:
+        assert self.ptr == self.max_size  # buffer has to be full before you can get
         self.ptr, self.path_start_idx = 0, 0
+        # the next two lines implement the advantage normalization trick
+        adv_mean = np.mean(self.adv_buf)
+        adv_std = np.std(self.adv_buf)
+        self.adv_buf = (self.adv_buf - adv_mean) / adv_std
+
+        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf,
+                    adv=self.adv_buf, logp=self.logp_buf)
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
         actual_adv_buf = np.array(self.adv_buf, dtype=np.float32)
         actual_adv_buf = actual_adv_buf[:actual_size]
