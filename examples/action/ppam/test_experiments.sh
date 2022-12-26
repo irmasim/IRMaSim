dd1c858e3aac6b79111d227950f8c2890cc674d4
jFompe <jaime.fompe@gmail.com>
2022-12-26 21:04:46 +0100

    Log tests in test directory

--- examples/action/ppam/test_experiments.sh
+++ examples/action/ppam/test_experiments.sh
@@ -1,3 +1,3 @@
-mkdir -p results
-rm -f agent.model results/*
-irmasim -nr 1 -im agent.model -om agent.model options_ppam_test.json
+mkdir -p test
+irmasim -nr 50 -im agent.model --phase test options_ppam_test.json
+python ../plotter.py -d ppam/test -r
