77ef06e0f6f4b3b17382027410a518f51948af2a
Esteban Stafford <esteban.stafford@unican.es>
2023-01-13 18:17:29 +0100

    Use fileinput to read input files

--- utils/swf2irmasim_workload.py
+++ utils/swf2irmasim_workload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
 import sys
+import fileinput
 import argparse as ap
 
 head = '''{
@@ -10,13 +11,13 @@ tail = '''\n   ]\n}'''
 
 
 def parse_workload_data(args):
-    file = open(args.swf_filename)
     print(head)
     sep = ""
-    for line in file:
-        #print(line)
+    for line in fileinput.input(args.swf_filename, openhook=fileinput.hook_compressed):
         if line[0] != ";":
             row = re.split(r'\s+',line)
+            if len(row) < 4:
+               continue
             if args.max_ntasks != None and int(row[4]) > args.max_ntasks:
                 row[4] = args.max_ntasks
             output = f'      {{"id": "job{row[0]}", "subtime": {row[1]}, "ntasks": {row[4]}, ' + \
@@ -31,7 +32,7 @@ def parse_workload_data(args):
 
 def main():
     parser = ap.ArgumentParser(description='Converts SWF traces to IRMaSim workload format')
-    parser.add_argument('swf_filename', type=str, nargs='?', help='File containing the SWF trace')
+    parser.add_argument('swf_filename', help='File containing the SWF trace')
     parser.add_argument('-f', '--freq', type=float, default=1e9, help='Frequency of the reference processor in Hz')
     parser.add_argument('--max-ntasks', type=int, help='Number of tasks to limit the jobs to')
 
