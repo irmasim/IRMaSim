13e9163d83c43c7dd76bafe49b3a10b79fd16957
jFompe <jaime.fompe@gmail.com>
2023-01-19 22:23:51 +0100

    Options for experiments

--- examples/action/ppam/test_experiments.sh
+++ examples/action/ppam/test_experiments.sh
@@ -1,3 +1,4 @@
 mkdir -p test
-irmasim -nr 50 -im agent.model --phase test options_ppam_test.json
+rm -f test/*
+irmasim -nr 20 -im agent.model --phase test options_ppam_test.json
 python ../plotter.py -d ppam/test -r
