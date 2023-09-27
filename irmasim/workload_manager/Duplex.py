203b85b7df052372f19b385fbec71f9dd71bc686
Esteban Stafford <esteban.stafford@unican.es>
2023-09-27 16:40:11 +0200

    Rewrote min-min and max-min algorithms with schedule class

--- irmasim/workload_manager/Duplex.py
+++ irmasim/workload_manager/Duplex.py
@@ -11,6 +11,17 @@ import sys
 if TYPE_CHECKING:
     from irmasim.Simulator import Simulator
 
+class Schedule:
+    def __init__(self, node: int, job: Job, completion_time: float, idle_cores: int = 0):
+        self.node = node
+        self.job = job
+        self.completion_time = completion_time
+        self.idle_cores = 0
+        self.running = False
+
+    def __repr__(self):
+        return "Schedule(node={}, job={}, completion_time={}, idle_cores={}, running={})".format(self.node, self.job.id, self.completion_time, self.idle_cores, self.running)
+
 class Duplex(WorkloadManager):
     def __init__(self, simulator: 'Simulator'):
         super(Duplex, self).__init__(simulator)
@@ -24,98 +35,103 @@ class Duplex(WorkloadManager):
             raise Exception("Duplex workload manager needs a valid scheduling option")
         print("Using option: ", self.option)
         mod = importlib.import_module("irmasim.platform.models." + Options().get()["platform_model_name"] + ".Node")
-        self.resources = self.simulator.get_resources(getattr(mod, "Node"))
+        self.nodes = self.simulator.get_resources(getattr(mod, "Node"))
 
-        self.speedups = []
-        self.base_sp_time = SortedList(self.resources, key=lambda node: node.children[0].mops_per_core)[0].children[0].mops_per_core
-        self.scheduling = []
-        self.cores_times = []
-        for i in range(0, len(self.resources)):
-            self.speedups.append(self.resources[i].children[0].mops_per_core/self.base_sp_time)
-            self.scheduling.append([])
-            self.cores_times.append([])
-            for j in range(0, len(self.resources[i].cores())):
-                self.cores_times[i].append(0)
-        self.idle_cores = []
-        for i in range(0, len(self.resources)):
-            self.idle_cores.append(len(self.resources[i].cores()))
-        self.running_jobs = []
+        self.lowest_mops = min([ node.children[0].mops_per_core for node in self.nodes ])
+        self.node_speedup = [ node.children[0].mops_per_core/self.lowest_mops for node in self.nodes]
+        self.node_queue = [ [] for node in self.nodes ]
 
     def on_job_submission(self, jobs: list):
-        self.jobs = jobs.copy()
-        min_min_planif = []
-        max_min_planif = []
+        min_min_scheduling = []
+        max_min_scheduling = []
 
         if self.option == "duplex" or self.option == "min_min":
-            planificating_jobs = SortedList(jobs, key=lambda job: job.req_time)
-            min_min_times = self.scheduler(planificating_jobs, min_min_planif)
+            sorted_jobs = SortedList(jobs, key=lambda job: job.req_time)
+            min_min_scheduling = self.create_schedule(sorted_jobs)
 
         if self.option == "duplex" or self.option == "max_min":
-            planificating_jobs = SortedList(jobs, key=lambda job: 0 - job.req_time)
-            max_min_times = self.scheduler(planificating_jobs, max_min_planif)
+            sorted_jobs = SortedList(jobs, key=lambda job: -job.req_time)
+            max_min_scheduling = self.create_schedule(sorted_jobs)
 
         if self.option == "duplex":
-            min_min_exec_time = max(map(lambda x: max(x), min_min_times))
-            max_min_exec_time = max(map(lambda x: max(x), max_min_times))
-            if min_min_exec_time <= max_min_exec_time:
+            min_min_final_time = max([ sch.completion_time for sch in min_min_scheduling ])
+            max_min_final_time = max([ sch.completion_time for sch in max_min_scheduling ])
+            if min_min_final_time < max_min_final_time:
                 self.option = "min_min"
             else:
                 self.option = "max_min"
+
         if (self.option == "min_min"):
-            self.cores_times = copy.deepcopy(min_min_times)
-            for n in range (0, len(self.scheduling)):
-                self.scheduling[n].extend(min_min_planif[n])
-           
-        if (self.option == "max_min"):
-            self.cores_times = copy.deepcopy(max_min_times)
-            for n in range (0, len(self.scheduling)):
-                self.scheduling[n].extend(max_min_planif[n])
-        for n in range(0, len(self.scheduling)):
-            self.schedule_next_job(n)
+            for sch in min_min_scheduling:
+                self.node_queue[sch.node].append(sch)
+        else:
+            for sch in max_min_scheduling:
+                self.node_queue[sch.node].append(sch)
+        self.schedule()
 
     def on_job_completion(self, jobs: list):
         for job in jobs:
-            for n in range(0, len(self.scheduling)):
-                if job in self.scheduling[n]:
-                    self.scheduling[n].pop(self.scheduling[n].index(job))
-                    self.idle_cores[n] += job.ntasks_per_node
-                    index_finished_job = self.running_jobs.index(job)
-                    self.running_jobs.pop(index_finished_job)
-                    self.schedule_next_job(n)
-                    break
+            for n in range(0, len(self.nodes)):
+                for sch in self.node_queue[n]:
+                    if sch.job == job:
+                        #print("Job ", job.id, " completed in node ", n, " at time ", self.simulator.simulation_time, " and the estimated completion time was ", sch.completion_time)
+                        self.node_queue[n].pop(self.node_queue[n].index(sch))
 
-    def schedule_next_job(self, n: int):
-        for job in self.scheduling[n]:
-            if job not in self.running_jobs:
-                if job.ntasks_per_node <= self.idle_cores[n]:
-                    self.idle_cores[n] -= job.ntasks_per_node
-                    self.running_jobs.append(job)
-                    for task in job.tasks:
-                        for core in self.resources[n].cores():
+        self.schedule()
+
+    def schedule(self):
+        for n in range(0, len(self.nodes)):
+            for sch in self.node_queue[n]:
+                node_idle_cores = self.nodes[n].count_idle_cores()
+                if not sch.running and sch.job.ntasks_per_node <= node_idle_cores:
+                    node_idle_cores -= sch.job.ntasks_per_node
+                    sch.running = True
+                    for task in sch.job.tasks:
+                        for core in self.nodes[n].cores():
                             if core.task == None:
                                 task.allocate(core.full_id())
                                 self.simulator.schedule([task])
-                                break
-                else:
-                    break
+                                break # Go to next task
+
 
     def on_end_step(self):
         pass
 
     def on_end_simulation(self):
         pass
-    
+
+    def create_schedule(self, jobs: list):
+        scheduling = []
+        for job in jobs:
+            best_completion_time = -1
+            for i in range(0, len(self.nodes)):
+                completion_time = self.calculate_completion_time(job, i, scheduling)
+                if completion_time > 0:
+                    if best_completion_time == -1 or completion_time < best_completion_time:
+                        best_completion_time = completion_time
+                        best_node = i
+            scheduling.append(Schedule(best_node, job, best_completion_time))
+        return scheduling
+
+    def calculate_completion_time(self, job: Job, node: int, scheduling: list):
+        if job.ntasks_per_node > self.nodes[node].count_cores():
+            return -1
+        if len(scheduling) != 0:
+            return scheduling[-1].completion_time + job.req_time / self.node_speedup[node]
+        if len(self.node_queue[node]) != 0:
+            return self.node_queue[node][-1].completion_time + job.req_time / self.node_speedup[node]
+        return self.simulator.simulation_time + job.req_time / self.node_speedup[node]
 
     def scheduler(self, jobs: list, scheduling: list):
         cores_times = copy.deepcopy(self.cores_times)
 
-        for n in range(0, len(self.resources)):
+        for n in range(0, len(self.nodes)):
             scheduling.append([])
 
         for j in range(0, len(jobs)):
             cores_temporal_times = copy.deepcopy(cores_times)
-            for n in range(0, len(self.resources)):
-                if len(self.resources[n].cores()) < jobs[j].ntasks_per_node:
+            for n in range(0, len(self.nodes)):
+                if len(self.nodes[n].cores()) < jobs[j].ntasks_per_node:
                     for c in range(len(cores_temporal_times[n])):
                         cores_temporal_times[n][c] = sys.maxsize
                     continue
@@ -124,13 +140,13 @@ class Duplex(WorkloadManager):
                 if(self.simulator.simulation_time > base_time):
                     base_time = self.simulator.simulation_time
                 for time_core in cores_task_times:
-                    exec_time = jobs[j].req_time/self.speedups[n]
-                    #exec_time = jobs[j].ops * jobs[j].opc * 1e-6 /(self.resources[n].cores()[cores_temporal_times[n].index(time_core)].mops)
+                    exec_time = jobs[j].req_time/self.node_speedup[n]
+                    #exec_time = jobs[j].ops * jobs[j].opc * 1e-6 /(self.nodes[n].cores()[cores_temporal_times[n].index(time_core)].mops)
                     cores_temporal_times[n][cores_temporal_times[n].index(time_core)] = base_time + exec_time
             min_time = min(map(lambda x: max(x), cores_temporal_times))
             if min_time == sys.maxsize:
                 continue
-            for n in range(0 , len(self.resources)):
+            for n in range(0 , len(self.nodes)):
                 if min_time in cores_temporal_times[n]:
                     cores_times[n] = cores_temporal_times[n].copy()
                     scheduling[n].append(jobs[j])
